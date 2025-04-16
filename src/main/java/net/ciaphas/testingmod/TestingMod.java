d4379dd31707ee543639d6c79237f41f8bc28827
TheCiaphas <thegideonstudio@gmail.com>
2025-04-16 14:36:35 +0100

    Item creation testing 50% complete

--- src/main/java/net/ciaphas/testingmod/TestingMod.java
+++ src/main/java/net/ciaphas/testingmod/TestingMod.java
@@ -1,6 +1,8 @@
 package net.ciaphas.testingmod;
 
 import com.mojang.logging.LogUtils;
+import net.ciaphas.testingmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,6 +28,8 @@ public class TestingMod {
     {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
 
@@ -41,7 +45,9 @@ public class TestingMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
