4dc65a5d2435f0d89dbcf0b1ab2958a8d238d08b
TheCiaphas <thegideonstudio@gmail.com>
2025-04-18 07:23:35 +0100

    DATAGEN PRE JSON DELETION

---
+++ src/main/java/net/ciaphas/testingmod/datagen/ModBlockTagGenerator.java
@@ -0,0 +1,50 @@
+package net.ciaphas.testingmod.datagen;
+
+import net.ciaphas.testingmod.TestingMod;
+import net.ciaphas.testingmod.block.ModBlocks;
+import net.ciaphas.testingmod.util.ModTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+import static net.ciaphas.testingmod.block.ModBlocks.END_SAPPHIRE_ORE;
+import static net.ciaphas.testingmod.block.ModBlocks.NETHER_SAPPHIRE_ORE;
+
+public class ModBlockTagGenerator extends BlockTagsProvider {
+    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, TestingMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+        this.tag(ModTags.Blocks.METAL_DETECTOR_VALUABLES)
+                .add(ModBlocks.SAPPHIRE_ORE.get()).add(ModBlocks.ADAMANTIUM_ORE.get()).addTag(Tags.Blocks.ORES);
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.SAPPHIRE_BLOCK.get(),
+                    ModBlocks.RAW_SAPPHIRE_BLOCK.get(),
+                    ModBlocks.RAW_ADAMANTIUM_BLOCK.get(),
+                    ModBlocks.ADAMANTIUM_BLOCK.get(),
+                    ModBlocks.ADAMANTIUM_ORE.get(),
+                    ModBlocks.SOUND_BLOCK.get(),
+                    ModBlocks.SAPPHIRE_ORE.get(),
+                    ModBlocks.END_SAPPHIRE_ORE.get(),
+                    ModBlocks.NETHER_SAPPHIRE_ORE.get(),
+                    ModBlocks.DEEPSLATE_SAPPHIRE_ORE.get());
+
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.SAPPHIRE_BLOCK.get()).add(ModBlocks.ADAMANTIUM_ORE.get()).add(ModBlocks.ADAMANTIUM_BLOCK.get())
+                .add(ModBlocks.RAW_ADAMANTIUM_BLOCK.get());
+        this.tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+        this.tag(BlockTags.NEEDS_STONE_TOOL)
+                .add(NETHER_SAPPHIRE_ORE.get());
+        this.tag(Tags.Blocks.NEEDS_NETHERITE_TOOL)
+                .add(END_SAPPHIRE_ORE.get());
+    }
+}
